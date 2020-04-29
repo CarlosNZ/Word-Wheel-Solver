90397e6ce0915e86e3f2bafcc18037ac78b2481d
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2020-04-29 12:26:31 +1200

    Firebase analytics

--- src/App.js
+++ src/App.js
@@ -2,6 +2,18 @@ import React, { Component } from "react";
 import "./App.scss";
 import * as ww from "./wordwheel";
 import Modal from "react-responsive-modal";
+import * as firebase from "firebase/app";
+import "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+};
+
+firebase.initializeApp(firebaseConfig);
 
 export default App;
 
